e8a7b4b3089381b51c7a0597090297fc8a91f0ec
drpsy03-create <dr.psy.03@gmail.com>
2025-10-06 13:37:32 +1000

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@
 export default {
   content: [
     "./index.html", 
+    "./main.tsx",
     "./App.tsx",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./services/**/*.{js,ts,jsx,tsx}",
-    "./utils/**/*.{js,ts,jsx,tsx}"
+    "./utils/**/*.{js,ts,jsx,tsx}",
+    "./globals.css"
   ],
   theme: {
     extend: {}
